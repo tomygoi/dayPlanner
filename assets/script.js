79b8930b382a79b5d5476193a02bef09c3d3caa2
Tony Trinh <tonytrinh911@gmail.com>
2023-02-01 23:29:09 -0500

    added function to get items from local storage but not working

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
- // TODO: Add code to display the current date in the header of the page.
  dayjs.extend(window.dayjs_plugin_advancedFormat);
  var currentDate = dayjs().format("dddd, MMMM Do");
  $("#currentDay").html(currentDate);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   $(document).ready(function() {
 
     $(".saveBtn").on("click", function() {
@@ -22,34 +11,43 @@
 
     })
 
-    // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-    function styleBlock () {
+    function styleBlock() {
       var currentTime = dayjs.hour();
 
       $(".time-block").each(function () {
         var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
+
         if (timeBlock < currentTime) {
+          $(this).addClass("past");
           $(this).removeClass("future");
           $(this).removeClass("present");
-          $(this).addClass("past");
+          
         }
         else if (timeBlock === currentTime) {
+          $(this).addClass("present");
           $(this).removeClass("future");
           $(this).removeClass("past");
-          $(this).addClass("present");
+          
         }
         else {
+          $(this).addClass("future");
           $(this).removeClass("past");
           $(this).removeClass("present");
-          $(this).addClass("future");
+          
         }
       })
     }
+
+    function storage() {
+      for (var i = 8; i <= 17; i++) {
+        $('#hour${hour}.description').val(localStorage.getItem('hour-${hour}'));
+      }
+    }
+
     styleBlock();
+    storage();
+
+
 
   })
 
